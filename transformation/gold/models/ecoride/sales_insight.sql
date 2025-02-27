2878bb203a850db98321d15c998b284dbb5fad80
Aman Preet Singh <chhabra.aps@gmail.com>
2025-02-27 04:51:45 +0000

    added dbt for sales insight in GOLD

---
+++ transformation/gold/models/ecoride/sales_insight.sql
@@ -0,0 +1,11 @@
+{% set nessie_branch = var('nessie_branch', 'main') %}
+
+SELECT
+    s.id as sale_id,
+    s.sale_date,
+    s.sale_price,
+    s.payment_method,
+    
+FROM {{ source('silver', 'sales') }} AT branch {{ nessie_branch }} s
+LEFT JOIN {{ source('silver', 'customers') }} AT branch {{ nessie_branch }} c ON c.id = s.customer_id
+LEFT JOIN {{ source('silver', 'vehicles') }}  AT branch {{ nessie_branch }} v ON s.vehicle_id = v.id
